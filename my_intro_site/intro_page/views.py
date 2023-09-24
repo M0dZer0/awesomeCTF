c5b8491d6d64c998129e0b0d3bbdbf74afd04c5c
zrq <zeroking@sjtu.edu.cn>
2023-09-24 16:51:34 +0800

    aaa

--- my_intro_site/intro_page/views.py
+++ my_intro_site/intro_page/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import zrq
+
+def zrqView(request):
+    all_zrq = zrq.objects.all()
+    return render(request, 'intro.html', {'zrq': all_zrq})
 
 def intro(request):
     return render(request, 'intro.html')
